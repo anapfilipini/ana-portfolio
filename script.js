89d30fe7340b63cd94639dda63cf7b8102b53bcc
Ana Filipini <anapfilipini@gmail.com>
2024-02-27 10:41:28 -0300

    vf commit

--- script.js
+++ script.js
@@ -33,50 +33,21 @@ linksInternos.forEach((link) => {
   link.addEventListener("click", scrollToSection);
 });
 
-(function () {
-  const links = document.querySelectorAll(".hover-this");
-  const cursors = document.querySelectorAll(".cursor");
+const sections = document.querySelectorAll(".js-scroll");
+const halfWindow = window.innerHeight * 0.6;
 
-  const animateIt = function (e) {
-    const span = this.querySelector(".hover-animation");
-    const { offsetX: x, offsetY: y } = e,
-      { offsetWidth: width, offsetHeight: height } = this,
-      move = 25,
-      xMove = (x / width) * (move * 2) - move,
-      yMove = (y / height) * (move * 2) - move;
-
-    span.style.transform = `translate(${xMove}px, ${yMove}px)`;
-
-    if (e.type === "mouseleave") span.style.transform = "";
-  };
-
-  const editCursors = (e) => {
-    const { clientX: x, clientY: y } = e;
-    cursors.forEach((cursor) => {
-      cursor.style.left = x + "px";
-      cursor.style.top = y + "px";
-    });
-  };
-
-  links.forEach((link) => {
-    link.addEventListener("mousemove", animateIt);
-    link.addEventListener("mouseleave", animateIt);
-  });
-  window.addEventListener("mousemove", editCursors);
-})();
-
-let sections = document.querySelectorAll("section");
-
-window.onscroll = () => {
-  sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add("show-animate");
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = sectionTop - halfWindow;
+    if (isSectionVisible < 0) {
+      section.classList.add("ativo");
     } else {
-      sec.classList.remove("show-animate");
+      section.classList.remove("ativo");
     }
   });
-};
+}
+
+animaScroll();
+
+window.addEventListener("scroll", animaScroll);

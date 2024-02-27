21324ab8621d006d899738b265496db70da0c308
Ana Filipini <anapfilipini@gmail.com>
2024-02-27 08:57:04 -0300

    v7 commit

--- script.js
+++ script.js
@@ -64,3 +64,19 @@ linksInternos.forEach((link) => {
   });
   window.addEventListener("mousemove", editCursors);
 })();
+
+let sections = document.querySelectorAll("section");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
+};
